f12507626e72c511915bc7b57540188d5613e798
Jakub Kozera <kozerakuba@gmail.com>
2020-03-21 11:38:29 +0100

    map popup styling

--- src/containers/main-dashboard/map/map-popup/MapPopup.jsx
+++ src/containers/main-dashboard/map/map-popup/MapPopup.jsx
@@ -8,12 +8,25 @@ export default function MapPopup({ country, state, confirmed, deaths, recovered,
     return (
         <Popup>
             <Link to={'/country/' + countryRoute}>
-
                 <div className="map-popup">
-                    {country} {state && ([{ state }])} <br />
-                    <span className="confirmed">• {confirmed} Confirmed</span> <br />
-                    <span className="recovered">• {recovered} Recovered</span> <br />
-                    <span className="deaths">• {deaths} Deaths</span>
+                    <div className="country-name">
+                        {country} {state && ([{ state }])}
+                    </div> 
+                    <div className="confirmed">
+                        <div className="label">Confirmed</div>
+                        <div className="amount">{confirmed}</div>
+                    </div>
+                    <div style={{clear: 'both'}}></div>
+                    <div className="recovered">
+                        <div className="label">Recovered</div>
+                        <div className="amount">{recovered}</div>
+                    </div>
+                    <div style={{clear: 'both'}}></div>
+                    
+                    <div className="deaths">
+                        <div className="label">Deaths</div>
+                        <div className="amount">{deaths}</div>
+                    </div>
                 </div>
             </Link>
         </Popup>
